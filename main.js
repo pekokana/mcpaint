4375b3295badd9c4a0a93e06cd220a968e5e14af
shinsugita <shinsugita@crooz.co.jp>
2016-07-04 10:28:46 +0900

    jqurey css

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ var mainWindow = null;
 require('crash-reporter').start();
 */
 const electron = require("electron");
-const {crashReporter} = require('electron');
+const {
+    crashReporter
+} = require('electron');
 crashReporter.start({
-  productName: 'YourName',
-  companyName: 'YourCompany',
-  submitURL: 'https://your-domain.com/url-to-submit',
-  autoSubmit: true
+    productName: 'YourName',
+    companyName: 'YourCompany',
+    submitURL: 'https://your-domain.com/url-to-submit',
+    autoSubmit: true
 });
 
 const app = electron.app;
@@ -21,18 +23,28 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
-    app.quit();
+    if (process.platform != 'darwin')
+        app.quit();
 });
 
 app.on('ready', function() {
 
-  // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  //mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    // ブラウザ(Chromium)の起動, 初期画面のロード
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    // mainWindow = new BrowserWindow({
+    //     width: 800,
+    //     height: 600,
+    //     'node-integration': false
+    // });
+    // デバッグするためのDevToolsを表示
+    mainWindow.webContents.openDevTools();
+    //mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 });
