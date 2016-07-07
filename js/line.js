b75793e86bab389100fe1511899d67524078558a
shinsugita <shinsugita@crooz.co.jp>
2016-07-07 15:02:28 +0900

    分ける line

--- js/line.js
+++ js/line.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // ラインの準備
+window.addEventListener("load", function() {
+    // キャンバス
+    var canvas = document.getElementById("canvas");
+    $('canvas').mousemove(function(e) {
+        var is_line = $('#line').is(':checked');
+        var is_wave_line = $('#wave-line').is(':checked');
+
+        if (is_line == true) {
+
+        }
+        if (is_wave_line == true) {
+
+        }
+    });
+})
