0766b7d77edab0c94098bf3b94ca6d94f39b7da4
shinsugita <shinsugita@crooz.co.jp>
2016-07-07 16:39:42 +0900

    line途中

--- js/line.js
+++ js/line.js
@@ -4,11 +4,19 @@
 window.addEventListener("load", function() {
     var start_x;
     var start_y;
+    var offset = 5;
     var flag = false;
+    var getImage;
+    var undoImage;
+    var brushSize = 1;
+    var alphaSize = 1;
+    var brushColor = '#000000';
 
     // キャンバス
     var canvas = document.getElementById("canvas");
-
+    if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+    }
     $('canvas').mousedown(function(e) {
         undoImage = context.getImageData(0, 0, $('canvas').width(), $('canvas').height());
         flag = true;
@@ -22,17 +30,57 @@ window.addEventListener("load", function() {
             return;
         }
 
+        var endX = e.pageX - $('canvas').offset().left - offset;
+        var endY = e.pageY - $('canvas').offset().top - offset;
+
         var is_line = $('#line').is(':checked');
         var is_wave_line = $('#wave-line').is(':checked');
 
         if (is_line == true) {
-
+            // 軌跡がいるかも？
+            // context.globalAlpha = alphaSize;
+            // context.beginPath();
+            // context.globalCompositeOperation = 'source-over';
+            // context.strokeStyle = brushColor;
+            // context.lineWidth = brushSize;
+            // context.lineJoin = 'miter';
+            // context.lineCap = 'butt';
+            // context.shadowBlur = 0;
+            // context.setTransform(1, 0, 0, 1, 0, 0);
+            // context.moveTo(start_x, start_y);
+            // context.lineTo(endX, endY);
+            // context.stroke();
+            // context.closePath();
         } else if (is_wave_line == true) {
 
         }
     });
 
-    window.addEventListener("mouseup", function() { // キャンバスでなくウィンドウに
+    window.addEventListener("mouseup", function(e) { // キャンバスでなくウィンドウに
+        var is_line = $('#line').is(':checked');
+        var is_wave_line = $('#wave-line').is(':checked');
+
+        var endX = e.pageX - $('canvas').offset().left - offset;
+        var endY = e.pageY - $('canvas').offset().top - offset;
+
+        if (is_line == true) {
+            context.globalAlpha = alphaSize;
+            context.beginPath();
+            context.globalCompositeOperation = 'source-over';
+            context.strokeStyle = brushColor;
+            context.lineWidth = brushSize;
+            context.lineJoin = 'miter';
+            context.lineCap = 'butt';
+            context.shadowBlur = 0;
+            context.setTransform(1, 0, 0, 1, 0, 0);
+            context.moveTo(start_x, start_y);
+            context.lineTo(endX, endY);
+            context.stroke();
+            context.closePath();
+        } else if (is_wave_line == true) {
+
+        }
+
         getImage = context.getImageData(0, 0, $('canvas').width(), $('canvas').height());
         flag = false;
     });
