d3c6b5698cca952802d20335b3f6b4f22c3c9b02
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-07-09 17:38:57 +0900

    楕円

--- js/elipse.js
+++ js/elipse.js
@@ -33,7 +33,7 @@ var elipseTool = (function() {
     };
 
     // 矩形オブジェクト
-    var _rectangle = {
+    var _elipse = {
         start_y: 0,
         start_x: 0,
         end_y: 0,
@@ -57,17 +57,17 @@ var elipseTool = (function() {
     }
 
     /**
-     * 矩形開始
+     * 円形開始
      * 
      * @param {any} e
      */
     function onMouseDown(e) {
         if (getElipse()) {
-            _rectangle.start_y = e.clientY;
-            var offsetX = parseInt($('canvas').css("left").replace("px", ""), 10);
+            _elipse.start_y = e.clientY;
+            var offset_x = parseInt($('canvas').css("left").replace("px", ""), 10);
             // _rectangle.startX = e.clientX;
             // console.log(offsetX);
-            _rectangle.start_x = e.clientX - offsetX;
+            _elipse.start_x = e.clientX - offset_x;
             _drawing_layer.addEventListener("mousemove", onMouseMove, false);
         }
     };
@@ -79,13 +79,31 @@ var elipseTool = (function() {
      */
     function onMouseMove(e) {
         if (getElipse()) {
+            document.body.style.cursor = "crosshair";
+
             _ctx_drawing_layer.clearRect(0, 0, _canvas_x, _canvas_y);
-            _rectangle.end_y = e.layerY - _rectangle.start_y;
-            _rectangle.end_x = e.layerX - _rectangle.start_x;
+            // _elipse.end_y = e.layerY - _elipse.start_y;
+            // _elipse.end_x = e.layerX - _elipse.start_x;
+            var end_y = e.layerY;
+            var end_x = e.layerX;
 
             var brush_color = picker.color;
             _ctx_drawing_layer.strokeStyle = brush_color;
-            _ctx_drawing_layer.strokeRect(_rectangle.start_x, _rectangle.start_y, _rectangle.end_x, _rectangle.end_y);
+           _ctx_drawing_layer.beginPath();
+            //    座標 (70, 70) が中心で半径60ピクセルの円を基準に、0°の位置から時計回りに360°回転
+            // _ctx_drawing_layer.arc(70, 70, 60, 0, Math.PI*2, false);
+            var a = end_x - _elipse.start_x;
+            var b = end_y - _elipse.start_y;
+            var center_x  = (end_x + _elipse.start_x) / 2;
+            var center_y =  (end_y + _elipse.start_y) / 2;
+
+            // 距離は直径
+             var diameter = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+             // 半径
+             var r = diameter / 2;
+
+             _ctx_drawing_layer.arc(center_x, center_y, r, 0, Math.PI*2, false);
+             _ctx_drawing_layer.stroke();
         }
     };
 
@@ -96,16 +114,29 @@ var elipseTool = (function() {
      */
     function onMouseUp(e) {
         if (getElipse()) {
+            document.body.style.cursor = "auto";
+            var end_y = e.layerY;
+            var end_x = e.layerX;
+
             // 塗りつぶしの色を決める
             var brush_color = picker.color;
             _ctx_display_layer.fillStyle = brush_color;
             // _ctx_display_layer.fillRect(_rectangle.start_x, _rectangle.start_y, _rectangle.end_x, _rectangle.end_y);
             _ctx_display_layer.beginPath();
-           _ctx_display_layer.arc(70, 70, 60, 0, Math.PI*2, false);
+            // _ctx_display_layer.arc(70, 70, 60, 0, Math.PI*2, false);
+            var a = end_x - _elipse.start_x;
+            var b = end_y - _elipse.start_y;
+            var center_x  = (end_x + _elipse.start_x) / 2;
+            var center_y =  (end_y + _elipse.start_y) / 2;
+
+             // 距離は直径
+             var diameter = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+             // 半径
+             var r = diameter / 2;
+            _ctx_display_layer.arc(center_x, center_y, r, 0, Math.PI*2, false);
            _ctx_display_layer.stroke();
   
-            _rectangle.clear();
-            _ctx_drawing_layer.clearRect(0, 0, _canvas_x, _canvas_y);
+            _elipse.clear();
             _drawing_layer.removeEventListener("mousemove", onMouseMove, false);
         }
     };
