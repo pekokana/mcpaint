8c16c7cadd0d1f02f015e08095adb822cab6d019
shinsugita <shinsugita@crooz.co.jp>
2016-07-11 17:49:57 +0900

    korea localize

--- localize/kr.js
+++ localize/kr.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
-    copy: "hello",
-    reload: "リロード",
+    title: "MC페인트",
+    quit: "종료",
+    copy: "카피",
+    reload: "리로드",
+    new_file: "신규 파일",
+    save: "보존",
+    resize: "사이즈 변경",
+    developer_tools: "개발툴",
+    full_screen: "풀 스크린 표시",
 };
