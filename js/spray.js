4d12d80919b33706ccbac667504307d8cdc2773b
shinsugita <shinsugita@crooz.co.jp>
2016-07-11 12:16:44 +0900

    sprayの位置

--- js/spray.js
+++ js/spray.js
@@ -9,21 +9,19 @@
  */
 var sprayTool = (function() {
     //キャンバスエレメント
-    var _displayLayer;
+    // var _displayLayer;
     var _drawingLayer;
     // 描画コンテキスト
     var _ctxDisplayLayer;
-    // 破線用
-    // var _ctxDrawingLayer;
 
     // キャンバスサイズ
-    var _canvasY;
-    var _canvasX;
+    // var _canvasY;
+    // var _canvasX;
 
     var _intervalId, // used to track the current interval ID
         _center; // the current center to spray
-    var radius = 100;
-    var density = 100;
+    var radius = 10;
+    var density = 10;
 
     /**
      * コンストラクタ
@@ -32,10 +30,10 @@ var sprayTool = (function() {
      * @param {any} _drawing
      */
     var app = function(_display, _drawing) {
-        _displayLayer = _display;
+        // _displayLayer = _display;
         _drawingLayer = _drawing;
-        _canvasY = _drawingLayer.clientHeight;
-        _canvasX = _drawingLayer.clientWidth;
+        // _canvasY = _drawingLayer.clientHeight;
+        // _canvasX = _drawingLayer.clientWidth;
         _ctxDisplayLayer = _display.getContext("2d");
         // _ctxDrawingLayer = _drawing.getContext("2d");
         // 破線
@@ -43,23 +41,6 @@ var sprayTool = (function() {
         // _ctxDrawingLayer.setLineDash(dashList);
         _drawingLayer.addEventListener("mousedown", onMouseDown, false);
         _drawingLayer.addEventListener("mouseup", onMouseUp, false);
-    };
-
-    // 矩形オブジェクト
-    var _rectangle = {
-        startY: 0,
-        startX: 0,
-        endY: 0,
-        endX: 0,
-        /**
-         * 
-         */
-        clear: function() {
-            this.startY = 0;
-            this.startX = 0;
-            this.endY = 0;
-            this.endX = 0;
-        }
     };
 
     /**
@@ -102,9 +83,8 @@ var sprayTool = (function() {
         }
     }
 
-
     /**
-     * 
+     * マウス押下
      * 
      * @param {any} e
      */
@@ -113,19 +93,14 @@ var sprayTool = (function() {
             var position = getPointFromEvent(e);
             // this.startDrawing(position);
             startDrawing(position);
-            // _rectangle.startY = e.clientY;
-            // var offsetX = parseInt($('canvas').css("left").replace("px", ""), 10);
-            // // _rectangle.startX = e.clientX;
-            // // console.log(offsetX);
-            // _rectangle.startX = e.clientX - offsetX;
-            // _drawingLayer.addEventListener("mousemove", onMouseMove, false);
+            _drawingLayer.addEventListener("mousemove", onMouseMove, false);
         }
     };
 
     // this.spray = function() {
     var spray = function() {
-        var centerX = _center.X,
-            centerY = _center.Y,
+        var centerX = _center.x,
+            centerY = _center.y,
             i;
 
         for (i = 0; i < density; i++) {
@@ -135,8 +110,6 @@ var sprayTool = (function() {
 
             // drawingCxt.fillRect(x, y, 1, 1);
             _ctxDisplayLayer.fillRect(x, y, 1, 1);
-            console.log(x);
-            console.log(y);
         }
     };
 
@@ -162,13 +135,6 @@ var sprayTool = (function() {
         if (getSpray()) {
             var position = getPointFromEvent(e);
             _center = position;
-            // _ctxDrawingLayer.clearRect(0, 0, _canvasX, _canvasY);
-            // _rectangle.endY = e.layerY - _rectangle.startY;
-            // _rectangle.endX = e.layerX - _rectangle.startX;
-
-            // var brush_color = picker.color;
-            // _ctxDrawingLayer.strokeStyle = brush_color;
-            // _ctxDrawingLayer.strokeRect(_rectangle.startX, _rectangle.startY, _rectangle.endX, _rectangle.endY);
         }
     };
 
@@ -182,14 +148,6 @@ var sprayTool = (function() {
             var position = getPointFromEvent(e);
             // this.finishDrawin(position);
             finishDrawing(position);
-            // 塗りつぶしの色を決める
-            // var brush_color = picker.color;
-            // _ctxDisplayLayer.fillStyle = brush_color;
-            // _ctxDisplayLayer.fillRect(_rectangle.startX, _rectangle.startY, _rectangle.endX, _rectangle.endY);
-
-            // _rectangle.clear();
-            // _ctxDrawingLayer.clearRect(0, 0, _canvasX, _canvasY);
-            // _drawingLayer.removeEventListener("mousemove", onMouseMove, false);
         }
     };
 
