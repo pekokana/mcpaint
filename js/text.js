11c511472930de38056bf21769349918236e254c
shinriyo <shinriyo@gmail.comm>
2016-07-12 08:55:47 +0900

    text create

--- js/text.js
+++ js/text.js
@@ -8,27 +8,46 @@ window.addEventListener("load", function() {
     var start_y;
     var offset = 5;
     var flag = false;
-    var getImage;
-    var undoImage;
+    var get_image;
+    var undo_image;
     var brush_size = 1;
-    var alphaSize = 1;
+    var alpha_size = 1;
     var brush_color = '#000000';
 
-    var input = new CanvasInput({
-        canvas: document.getElementById('drawing'),
-        fontSize: 18,
-        fontFamily: 'Arial',
-        fontColor: '#212121',
-        fontWeight: 'bold',
-        width: 300,
-        padding: 8,
-        borderWidth: 1,
-        borderColor: '#000',
-        borderRadius: 3,
-        boxShadow: '1px 1px 0px #fff',
-        innerShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
-        placeHolder: 'Enter message here...'
-    });
+    var drawing = document.getElementById('drawing');
+
+    // テキスト入力のツール
+    var input;
+
+    /**
+     * テキスト入力のツールを作る
+     * 
+     * @param {any} x
+     * @param {any} y
+     */
+    function createInput(x, y) {
+        input = new CanvasInput({
+            x: x,
+            y: y,
+            canvas: drawing,
+            fontSize: 18,
+            fontFamily: 'Arial',
+            fontColor: '#212121',
+            fontWeight: 'bold',
+            width: 300,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000',
+            borderRadius: 3,
+            boxShadow: '1px 1px 0px #fff',
+            innerShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
+            placeHolder: 'Enter message here...'
+        });
+    }
+
+    // 一旦隠す
+    // drawing.style.display = "none";
+    // input.destroy();
 
     // キャンバス
     var canvas = document.getElementById("canvas");
@@ -37,11 +56,15 @@ window.addEventListener("load", function() {
     }
 
     $('canvas').mousedown(function(e) {
+        if(flag) return;
+
         // undoImage = context.getImageData(0, 0, $('canvas').width(), $('canvas').height());
-        // flag = true;
-        // start_x = e.pageX - $(this).offset().left - offset;
-        // start_y = e.pageY - $(this).offset().top - offset;
+        flag = true;
+        start_x = e.pageX - $(this).offset().left - offset;
+        start_y = e.pageY - $(this).offset().top - offset;
         // context.strokeText("text", start_x, start_y)
+        createInput(start_x, start_y);
+        // drawing.style.display = "block";
         return false; // for chrome
     });
 
@@ -56,20 +79,6 @@ window.addEventListener("load", function() {
         var is_text = $('#text').is(':checked');
 
         if (is_text == true) {
-            // 軌跡がいるかも？
-            // context.globalAlpha = alphaSize;
-            // context.beginPath();
-            // context.globalCompositeOperation = 'source-over';
-            // context.strokeStyle = brush_color;
-            // context.lineWidth = brush_size;
-            // context.lineJoin = 'miter';
-            // context.lineCap = 'butt';
-            // context.shadowBlur = 0;
-            // context.setTransform(1, 0, 0, 1, 0, 0);
-            // context.moveTo(start_x, start_y);
-            // context.lineTo(endX, endY);
-            // context.stroke();
-            // context.closePath();
         }
     });
 
@@ -80,22 +89,9 @@ window.addEventListener("load", function() {
         var endY = e.pageY - $('canvas').offset().top - offset;
 
         if (is_text == true) {
-            context.globalAlpha = alphaSize;
-            context.beginPath();
-            context.globalCompositeOperation = 'source-over';
-            context.strokeStyle = brush_color;
-            context.lineWidth = brush_size;
-            context.lineJoin = 'miter';
-            context.lineCap = 'butt';
-            context.shadowBlur = 0;
-            context.setTransform(1, 0, 0, 1, 0, 0);
-            context.moveTo(start_x, start_y);
-            context.lineTo(endX, endY);
-            context.stroke();
-            context.closePath();
         }
 
-        getImage = context.getImageData(0, 0, $('canvas').width(), $('canvas').height());
-        flag = false;
+        // get_image = context.getImageData(0, 0, $('canvas').width(), $('canvas').height());
+        // flag = false;
     });
 })
